f1147842fb9c1e56511e2fb6ffd031c4e482675b
Muhammad Zaid Mohsin <147637998+ZaidMohsin457@users.noreply.github.com>
2025-02-16 20:42:02 +0500

    added history page

--- djangoapp/urls.py
+++ djangoapp/urls.py
@@ -3,4 +3,5 @@ from djangoapp import views
 
 urlpatterns = [
     path('',views.index,name='index'),
+    path('history/',views.history,name='History'),
 ]
--- djangoapp/views.py
+++ djangoapp/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request,'home.html')
+
+def history(request):
+    return render(request,'history.html')
