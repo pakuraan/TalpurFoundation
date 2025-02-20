473ada372f1423441f661d0af9915420b2836073
ammartalpur <ammartalpur69@gmail.com>
2025-02-21 02:55:52 +0500

    fix / route

--- djangoapp/urls.py
+++ djangoapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from djangoapp import views
 
 urlpatterns = [
-    path('home/',views.index,name='index'),
+    path('/',views.index,name='index'),
     path('history/',views.history,name='History'),
     path('leadership/',views.leadership,name='Leadership'),
     path('sites/',views.sites,name='Sites'),
